850083667f692d1e3795c1e21f29e738b425c6ee
biancamatt <20231818@nbsc.edu.ph>
2024-10-07 22:27:38 +0800

    added additional text in the map container

--- applet-2.js
+++ applet-2.js
@@ -9,7 +9,7 @@ class LeafletMap {
     initTileLayer() {
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> Francis Adrian Idul | Sample of must visit eatery around NBSC vicinity'
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> NBSC Map'
         }).addTo(this.map);
     }
 
