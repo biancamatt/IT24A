06bc16128067ac869acab657406ab650b0dbee7a
biancamatt <20231818@nbsc.edu.ph>
2024-10-17 10:14:38 +0800

    added constructor in class weatherapp

--- applet-5.js
+++ applet-5.js
@@ -56,7 +56,7 @@ class WeatherService extends WeatherApp {
             alert('Please enter a city name.');
         }
     }
-    
+
 
     async fetchWeatherByLocation() {
         if (navigator.geolocation) {
