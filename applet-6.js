53dd34ec17110805bdf305ac2af50fd17b1362bf
biancamatt <20231818@nbsc.edu.ph>
2024-11-04 13:09:56 +0800

    added consr taskTest

--- applet-6.js
+++ applet-6.js
@@ -4,7 +4,7 @@ class todoList{
         this.addButton = document.getElementById('addButton');
         this.todoInput = document.getElementById('todoInput');
         this.todoList = document.getElementById('todoList');
-        
+
 
         this.addButton.addEventListener('click', () => this.addOrUpdateTask());
         this.todoList.addEventListener('click', (e) => {
@@ -14,6 +14,7 @@ class todoList{
             if (action) this[action + 'Task'](e);
         });
     }
+    
 
     addOrUpdateTask() {
         const taskText = this.todoInput.value.trim();
