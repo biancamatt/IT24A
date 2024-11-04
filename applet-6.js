5ec3ce36cf5880cba870c50ee89f9f0c9321c7ae
biancamatt <20231818@nbsc.edu.ph>
2024-11-04 13:09:08 +0800

    added constructors

--- applet-6.js
+++ applet-6.js
@@ -4,6 +4,7 @@ class todoList{
         this.addButton = document.getElementById('addButton');
         this.todoInput = document.getElementById('todoInput');
         this.todoList = document.getElementById('todoList');
+        
 
         this.addButton.addEventListener('click', () => this.addOrUpdateTask());
         this.todoList.addEventListener('click', (e) => {
